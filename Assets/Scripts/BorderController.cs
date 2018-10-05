65ae1eb351ab85181a223897118d714c48a11c30
Vincent <vincent.guillon@hotmail.fr>
2018-10-05 19:55:26 -0400

    Player movement and camera switch implemented

---
+++ Assets/Scripts/BorderController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BorderController : MonoBehaviour
+{
+	public Camera OldCamera;
+	public Camera NextCamera;
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		Debug.Log("Collision detected");
+		OldCamera.gameObject.SetActive(false);
+		NextCamera.gameObject.SetActive(true);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float Speed = 0.1f;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x-=Speed;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.x+=Speed;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.UpArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y+=Speed;
+			this.transform.position = position;
+		}
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			Vector3 position = this.transform.position;
+			position.y-=Speed;
+			this.transform.position = position;
+		}
+	}
+}
