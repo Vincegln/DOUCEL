cb7046f69e9fd7b21545fd123de90019f176d17e
Unknown <melanie.lachaise@gmail.com>
2018-10-06 11:14:20 -0400

    Timer

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+	public float TimeLeft = 1200.0f;
+	private int _hoursText;
+	private int _minutesText;
+	public Text TimerText;
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		TimeLeft -= Time.deltaTime;
+		_hoursText = (int) ((TimeLeft*72) / 3600);
+		_minutesText = (int) (TimeLeft*72 - (_hoursText * 3600)) / 60;
+		TimerText.text = _hoursText + ":" + _minutesText;
+
+	}
+}
