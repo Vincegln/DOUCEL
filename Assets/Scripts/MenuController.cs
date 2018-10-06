767f8f71182a060cafdd37d2cba3292e4447d369
Vincent <vincent.guillon@hotmail.fr>
2018-10-06 19:09:23 -0400

    Menu & Credits done

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -8,12 +8,16 @@ public class MenuController : MonoBehaviour {
 
 	public Button PlayButton;
 	public Button ExitButton;
+	public Button CreditsButton;
+	public Button ReturnButton;
 
 	// Use this for initialization
 	private void Start()
 	{
 		PlayButton.onClick.AddListener(PlayHandleClick);
 		ExitButton.onClick.AddListener(ExitHandleClick);
+		CreditsButton.onClick.AddListener(CreditsHandleClick);
+		ReturnButton.onClick.AddListener(ReturnHandleClick);
 	}
 
 	public void PlayHandleClick()
@@ -24,5 +28,17 @@ public class MenuController : MonoBehaviour {
 	public void ExitHandleClick()
 	{
 		Application.Quit();
+	}
+	
+	private void CreditsHandleClick()
+	{
+		CreditsButton.transform.parent.gameObject.SetActive(false);
+		ReturnButton.transform.parent.gameObject.SetActive(true);
+	}
+	
+	private void ReturnHandleClick()
+	{
+		ReturnButton.transform.parent.gameObject.SetActive(false);
+		CreditsButton.transform.parent.gameObject.SetActive(true);
 	}
 }
