71c5be83e09fd19cc24fa00e1a8dbcadb935f7cc
Vincent <vincent.guillon@hotmail.fr>
2018-10-06 17:13:48 -0400

    Menu implemented

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MenuController : MonoBehaviour {
+
+	public Button PlayButton;
+	public Button ExitButton;
+
+	// Use this for initialization
+	private void Start()
+	{
+		PlayButton.onClick.AddListener(PlayHandleClick);
+		ExitButton.onClick.AddListener(ExitHandleClick);
+	}
+
+	public void PlayHandleClick()
+	{
+		SceneManager.LoadScene("MainMap");
+	}
+
+	public void ExitHandleClick()
+	{
+		Application.Quit();
+	}
+}
