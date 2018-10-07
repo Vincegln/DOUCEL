9b2e71b6f51da9955ad92086175c1ee55e6bef01
Vincent <vincent.guillon@hotmail.fr>
2018-10-07 00:49:36 -0400

    In Game Menu implemented

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Internal.Experimental.UIElements;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
@@ -15,22 +17,64 @@ public class GameController : MonoBehaviour
 	public List<Image> ItemSlots;
 	public List<Text> ItemNames;
 	public int NbOfItems = 0;
-	
+	private bool _inGameMenu = false;
+	private PlayerController _playerScript;
+	private float _playerSpeed;
+	public Button ExitButton;
+	public Button ReturnButton;
+	private bool _returnButtonClicked = false;
+	public GameObject MenuPanel;
+
+	private void Start()
+	{
+		ExitButton.onClick.AddListener(ExitHandleClick);
+		ReturnButton.onClick.AddListener(ReturnHandleClick);
+		_playerScript = GameObject.FindWithTag("Player").GetComponent<PlayerController>();
+	}
+
 	// Update is called once per frame
 	void Update ()
 	{
-		//TimeLeft -= Time.deltaTime;
-		TimeElapsed += Time.deltaTime;
-		_hoursText = (int) ((TimeElapsed*72) / 3600);
-		_minutesText = (int) (TimeElapsed*72 - (_hoursText * 3600)) / 60;
-		if (_hoursText + 13 > 24)
+		if (Input.GetKeyDown(KeyCode.Escape) || _returnButtonClicked)
 		{
-			_hoursText -= 11;
+			if (!_inGameMenu)
+			{
+				_playerSpeed = _playerScript.Speed;
+				_playerScript.Speed = 0.0f;
+				MenuPanel.SetActive(true);
+			}
+			else if (_inGameMenu)
+			{
+				MenuPanel.SetActive(false);
+				_playerScript.Speed = _playerSpeed;
+				_returnButtonClicked = false;
+			}
+			_inGameMenu = !_inGameMenu ;
 		}
-		else
-		{
-			_hoursText += 13;}
-		TimerText.text = _hoursText + ":" + _minutesText.ToString("D2");
 
+		if (!_inGameMenu)
+		{
+			TimeElapsed += Time.deltaTime;
+			_hoursText = (int) ((TimeElapsed*72) / 3600);
+			_minutesText = (int) (TimeElapsed*72 - (_hoursText * 3600)) / 60;
+			if (_hoursText + 13 > 24)
+			{
+				_hoursText -= 11;
+			}
+			else
+			{
+				_hoursText += 13;}
+			TimerText.text = _hoursText + ":" + _minutesText.ToString("D2");
+		}
+	}
+	
+	public void ExitHandleClick()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+	
+	private void ReturnHandleClick()
+	{
+		_returnButtonClicked = true;
 	}
 }
